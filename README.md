d1e7fa1161f12e0ba1a2b4d6e092624c62ac7dc1
JHONATANGARCIAXD <jhonatangarcia@colegioguanenta.edu.co>
2022-04-29 06:52:49 -0500

    Desarrollo: Comparacion edad

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ejercicio 1:
+
+## Programa para verificar si una persoa es mayor de edad
+
+## DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ comparacion.py
@@ -0,0 +1,17 @@
+
+
+
+# input 
+
+edad=int(input("Digite la edad:"))
+
+
+# processing
+
+if edad>=18:
+    print("La persona es Mayor de Edad")
+
+else:
+    print("La persona es Menor de Edad")
+
+
